2635e954360d6c336c648e4d1b79e652cd10f244
ogoldthwaite <ogoldthwaite@10.10.122.21>
2016-09-08 10:26:22 -0400

    Initial Commit

---
+++ Goldthwaite, Owen/src/runtimeanalyser/printtest.java
@@ -0,0 +1,16 @@
+package runtimeanalyser;
+
+public class printtest {
+
+	public static void main(String[] args) 
+	{
+		
+	    System.out.println(1);
+		
+		// int [] stuff = new int[3]; //array syntax
+	
+		
+
+	}
+
+}
