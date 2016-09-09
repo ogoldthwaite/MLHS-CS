d29b436d65cfbc61aed9ae99298a9e234a041ca6
ogoldthwaite <ogoldthwaite@10.10.122.63>
2016-09-09 10:31:24 -0400

    Two Computer In Class Git Practice

--- Goldthwaite, Owen/src/runtimeanalyser/printtest.java
+++ Goldthwaite, Owen/src/runtimeanalyser/printtest.java
@@ -9,8 +9,9 @@ public class printtest {
 		
 		// int [] stuff = new int[3]; //array syntax
 	
-		
+		/*  int[][] array2d = new int [4][2]; row followed by column                 */
 
+	    // THIS IS A CHANGE!!!
 	}
 
 }
