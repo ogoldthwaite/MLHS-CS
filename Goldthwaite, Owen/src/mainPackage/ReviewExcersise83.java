644e063662a29d75f74e5a9ee1698838a9b88490
Owen <Owen@MSI-GS70>
2016-09-10 11:04:19 -0400

    First version 8.3 review excersises

--- Goldthwaite, Owen/src/mainPackage/ReviewExcersise83.java
+++ Goldthwaite, Owen/src/mainPackage/ReviewExcersise83.java
@@ -4,14 +4,38 @@ public class ReviewExcersise83 {
 
 	public static void main(String[] args) 
 	{
-	// ARRAYLIST SYNTAX ArrayList <ClassName> <ListName> = new ArrayList <ClassName> ();
-		int max; int min;
-        int [] account = new int [10];
-        for(int i=0; i<account.length; i++)
-        {
-        // Generate random number for each value
-        }
-	System.out.println("test");
+	int[] maxminvalues = new int [2];
+	maxminvalues=maxMin();	
+		
+	 System.out.print("Max Value is..");   System.out.println(maxminvalues[0]);  //max val
+	 System.out.print("Min Value is..");   System.out.println(maxminvalues[1]);  //min val
+	
+	//MOVE THESE PRINT STATEMENT TO METHOD
+	
 	}
 
+	public static int[] maxMin()
+	{
+	  int max = Integer.MIN_VALUE; int min = Integer.MAX_VALUE; int foo=0;
+      int [] account = new int [10]; int[] maxminarr = new int [2];
+        for(int i=0; i<account.length; i++)
+        {
+        account[i] = (int)(100*Math.random());
+        if (account[i]>max) max = account[i];
+        if (account[i]<min) min = account[i];
+        }
+		maxminarr[0]=max;
+        maxminarr[1]=min;
+      return maxminarr;  
+	}
+	
+	
+	public static void stringLoop()
+	{
+	   
+		
+	}
+	
+	
 }
+// ARRAYLIST SYNTAX   ArrayList <Type> <ListName> = new ArrayList <Type> ();
---
+++ Goldthwaite, Owen/src/mainPackage/TestClass.java
@@ -0,0 +1,12 @@
+package mainPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) 
+	{
+	
+		System.out.println((int)(100*Math.random()));
+
+	}
+
+}
