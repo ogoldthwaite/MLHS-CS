0bee72b8ec4c44f2fe7a46b281d3176de1d859dc
ogoldthwaite <ogoldthwaite@10.10.126.211>
2016-09-19 10:59:32 -0400

    LinkList excersises start

---
+++ Goldthwaite, Owen/src/linkListExcersises/LinkListTester.java
@@ -0,0 +1,21 @@
+package linkListExcersises;
+
+import java.util.*;
+
+public class LinkListTester {
+
+	public static void main(String[] args) 
+	{
+		List employees = Collections.synchronizedList(new LinkedList<String>());
+		
+	}
+
+	public static void downsize(LinkedList<String> staff)
+	{
+		
+	}
+	
+	
+	
+	
+}
