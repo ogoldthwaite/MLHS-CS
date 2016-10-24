ae4b299d47405de673d255b075d97edaab25ca4f
ogoldthwaite <ogoldthwaite@10.10.126.211>
2016-10-24 11:00:00 -0400

    Directory Traverse work

---
+++ Goldthwaite, Owen/src/recursiveFileDirectory/DirectoryTraverse.java
@@ -0,0 +1,31 @@
+package recursiveFileDirectory;
+
+import java.io.File;
+
+public class DirectoryTraverse {
+
+	public static void main(String[] args) 
+	{
+		String path = "/users/ogoldthwaite/documents";
+		printDirectory(path);
+
+	}
+
+	public static void printDirectory(String path)	
+	{
+		File coolFile = new File(path);
+
+		if (coolFile.isDirectory())
+		{
+			String files[]; 	
+			files = coolFile.listFiles();
+			for(int i = 0; i < files.length; i++)
+			{
+				printDirectory(files[i]);
+			}
+			
+		}
+		else System.out.println(coolFile.getName());
+	}
+
+}
