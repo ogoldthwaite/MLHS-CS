954b8f19cb8ea4593f61167fa42a2d526b364278
ogoldthwaite <ogoldthwaite@10.10.126.29>
2016-11-30 10:58:34 -0500

    BinarySearchTree

--- Goldthwaite, Owen/src/binarySearchTree/BinarySearchTree.java
+++ Goldthwaite, Owen/src/binarySearchTree/BinarySearchTree.java
@@ -22,7 +22,7 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends LinkedBin
     	return contains(this.root, element);
     }
     
-    private boolean contains(TreeNode<E> node, E element) //rewrite this stuff
+    private boolean contains(TreeNode<E> node, E element) //rewrite this stuff. Hopefully I remember
     {
     	if(node == null)
         	return false;
@@ -39,10 +39,8 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends LinkedBin
     @Override
     public void add(E element)
     {
-    	TreeNode<E> coolNode = new TreeNode<E>();
-    	coolNode.value = element;
-    	
-    	add(getRoot(), element);
+    	root = add(getRoot(), element);
+    	size++;
     }
     
     protected TreeNode<E> constructNewNode(E element)
@@ -59,83 +57,65 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends LinkedBin
      * @return the value to which the pointer previously pointing to node should point
      */
     protected TreeNode<E> add(TreeNode<E> node, E element)
-    {
-    	//Fist thing added is the root value, so this if statement is a silly goose
-    	
-    	if(size == 0)
-    		setRoot(constructNewNode(element));
-    	
-    	if(node.left == null && element.compareTo(node.value) < 0)
-    	{	
-    		node.left = constructNewNode(element);
-    		return node;
-    	}
-    		
-    	if(node.right == null && element.compareTo(node.value) >= 0)
-    	{
-    		node.right = constructNewNode(element);
-    		return node;
-    	}
+    {	
+    	if(node == null)
+    		return constructNewNode(element);
     	
     	if(element.compareTo(node.value) < 0)
-    		return add(node.left, element);
+    		node.left = add(node.left, element);
+    	else
+    		node.right = add(node.right, element);
     	
-    	if(element.compareTo(node.value) >= 0)
-    		return add(node.right, element);
-    	
-    	size++;
     	return node;
-    	
-    	
     }
     
-    public boolean remove(E element)
-    {
-        
-    }
-    
-    /**
-     * @param node the root of the subtree from which to remove the element
-     * @param element the element to be removed
-     * @return the value to which the pointer previously pointing to node should point
-     */
-    protected TreeNode<E> remove(TreeNode<E> node, E element)
-    {
-        
-    }
-    
-    public E min()
-    {
-        
-    }
-    
-    private TreeNode<E> min(TreeNode<E> node)
-    {
-        
-    }
-    
-    public E max()
-    {
-        
-    }
-    
-    protected TreeNode<E> rotateRight(TreeNode<E> node)
-    {
-        
-    }
-    
-    protected TreeNode<E> rotateLeft(TreeNode<E> node)
-    {
-        
-    }
-    
-    protected TreeNode<E> rotateRightLeft(TreeNode<E> node)
-    {
-        
-    }
-    
-    protected TreeNode<E> rotateLeftRight(TreeNode<E> node)
-    {
-        
-    }
-}
+//    public boolean remove(E element)
+//    {
+//        
+//    }
+//    
+//    /**
+//     * @param node the root of the subtree from which to remove the element
+//     * @param element the element to be removed
+//     * @return the value to which the pointer previously pointing to node should point
+//     */
+//    protected TreeNode<E> remove(TreeNode<E> node, E element)
+//    {
+//        
+//    }
+//    
+//    public E min()
+//    {
+//        
+//    }
+//    
+//    private TreeNode<E> min(TreeNode<E> node)
+//    {
+//        
+//    }
+//    
+//    public E max()
+//    {
+//        
+//    }
+//    
+//    protected TreeNode<E> rotateRight(TreeNode<E> node)
+//    {
+//        
+//    }
+//    
+//    protected TreeNode<E> rotateLeft(TreeNode<E> node)
+//    {
+//        
+//    }
+//    
+//    protected TreeNode<E> rotateRightLeft(TreeNode<E> node)
+//    {
+//        
+//    }
+//    
+//    protected TreeNode<E> rotateLeftRight(TreeNode<E> node)
+//    {
+//        
+//    }
+	}
