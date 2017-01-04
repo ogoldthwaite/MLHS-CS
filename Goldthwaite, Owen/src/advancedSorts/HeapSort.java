9138c96b1466116e821116d9fc87c34300b41c9f
ogoldthwaite <ogoldthwaite@10.10.123.208>
2017-01-04 10:59:00 -0500

    heap

--- Goldthwaite, Owen/src/advancedSorts/HeapSort.java
+++ Goldthwaite, Owen/src/advancedSorts/HeapSort.java
@@ -1,15 +1,36 @@
 package advancedSorts;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 
 public class HeapSort
 {
+	private static int heapSize = 0;
+	private static ArrayList<Integer> elements;
+	
 	public static <T> void sort(T[] a, Comparator<T> c)
 	{
 		//c.compare(element1, element2); How you compare stuff!
+		//elements = a;
+		for(int i = 1; i < a.length; i++)
+		{
+			int pIndex = getParentIndex(i);
+			int k = i;
+			
+		while(c.compare(a[k], a[pIndex]) > 0 && pIndex > -1)
+		{
+			if (c.compare(a[k], a[pIndex]) > 0)
+			{
+				swap(a, k, pIndex);
+				k = pIndex;
+				pIndex = getParentIndex(k); //change this k I think
+			}
+		}
+			
+			heapSize++;
+		}
 		
-		
-		
+		System.out.println(a);
 		
 		
 	}
@@ -31,6 +52,45 @@ public class HeapSort
 	        return 2 * parentIndex + (left ? 1 : 2);
 	    }
 
+	 private void fixHeap(int nodeIndex)
+		{
+			int leftChildIndex = getChildIndex(nodeIndex, true);
+			int rightChildIndex = getChildIndex(nodeIndex, false);
+			//E min = elements.get(nodeIndex);
+			//int minIndex = nodeIndex;
 
+			
+			if(elements.size() != 0)
+			{
+				int min = elements.get(nodeIndex);
+				int minIndex = nodeIndex;
+				
+				if(leftChildIndex < elements.size())
+				{
+					fixHeap(leftChildIndex);
+					if(elements.get(leftChildIndex).compareTo(min) < 0)
+					{
+						min = elements.get(leftChildIndex);
+						minIndex = leftChildIndex;
+					}
+				}
+
+				if(rightChildIndex < elements.size())
+				{
+					fixHeap(rightChildIndex);
+					if(elements.get(rightChildIndex).compareTo(min) < 0)
+					{
+						min = elements.get(rightChildIndex);
+						minIndex = rightChildIndex;
+					}
+
+				}	
+					if(minIndex != nodeIndex)
+					{	
+						elements.set(minIndex, elements.get(nodeIndex));
+						elements.set(nodeIndex, min);
+					} 
+			} 	
+		}
 
 }
