e319da6bdafae37b3dc422b3fd29827f715102d4
ogoldthwaite <ogoldthwaite@10.10.124.159>
2017-01-11 10:57:58 -0500

    Server Simple

--- Goldthwaite, Owen/src/simpleServer/SendChat.java
+++ Goldthwaite, Owen/src/simpleServer/SendChat.java
@@ -6,20 +6,46 @@ public class SendChat implements Runnable
 {
 	private Thread t;
 	private Scanner hostKey;
+	private String nickName;
 	
 	public SendChat(Scanner scan)
 	{
 		hostKey = scan;	
+		nickName = "";
 		t = new Thread(this);
 		t.start();	
 	}
 	
 	public void run()
 	{
-		System.out.println(hostKey.next());	
+		while( ! Thread.interrupted())
+		{
+		
+		String text = hostKey.next();
+		
+//		if(5 <= text.length() && text.substring(0, 5).equals("/nick"))
+		if(text.startsWith("/nick"))
+		{
+			//nickName = text.substring("/nick ".length());
+			
+			//nickName = "bob";
+		}
+		
+		
+		
+		if(nickName.equals(""))
+			System.out.println(text);
+		else	
+		    System.out.println(nickName + ": " + text);
+		
+		}
+		
 	}
 	
-	
+	public void annihilate()
+	{
+		t.interrupt();
+	}
 	
 	
 }
