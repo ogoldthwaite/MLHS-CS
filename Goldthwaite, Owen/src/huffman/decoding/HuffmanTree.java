8a8d84f4860e0b4f8929b13c506e1507e176fd2a
ogoldthwaite <ogoldthwaite@10.10.125.110>
2017-01-31 10:57:44 -0500

    Huffman Tree stuff

--- Goldthwaite, Owen/src/huffman/decoding/HuffmanTree.java
+++ Goldthwaite, Owen/src/huffman/decoding/HuffmanTree.java
@@ -2,51 +2,58 @@ package huffman.decoding;
 
 public class HuffmanTree
 {
-    private TreeNode root;
-    
-    public String decodeMessage(String encodedMessage)
-    {
-        String msg = "";
-    	
-    	for(int i = 0; i < encodedMessage.length(); i++)
-        {
-        	if(encodedMessage.substring(i, i+1).equals(0))
-        		root = root.left;
-        	
-        	if(encodedMessage.substring(i, i+1).equals(1))
-        		root = root.right;
-        	
-        	
-        	
-        	
-        }
-    	
-    	
-    	
-    }
-    
-    public void insert(char letter, String encoding)
-    {
-        root = insert(letter, encoding, root);
-    }
+	private TreeNode root;
 
-    // returns new root
-    private static TreeNode insert(char letter, String encoding, TreeNode node)
-    {
-    	
-    		if(encoding.length() == 0)
-    		{
-    			node.letter = letter+"";
-    			return node;
-    		}
-    	
-    		if(encoding.substring(0,1).equals(0))
-    			node = node.left;
-    			
-    		if(encoding.substring(0, 1).equals(1))
-    			node = node.right;
-    		
-    		return insert(letter, encoding.substring(1), node);
-    		
-    }
+	public String decodeMessage(String encodedMessage)
+	{
+		String msg = "";
+		TreeNode node = root;
+		int i = 0;
+
+		for(int k = 0; k < encodedMessage.length(); k++)
+		{
+
+			while(node != null)
+			{
+				if(encodedMessage.substring(i, i+1).equals(0))
+					node = node.left;
+
+				if(encodedMessage.substring(i, i+1).equals(1))
+					node = node.right;
+
+				i++;
+			}
+
+			msg += node.letter;
+			node = root;
+		}
+
+		return msg;
+
+	}
+
+	public void insert(char letter, String encoding)
+	{
+		root = insert(letter, encoding, root);
+	}
+
+	// returns new root
+	private static TreeNode insert(char letter, String encoding, TreeNode node)
+	{
+
+		if(encoding.length() == 0)
+		{
+			node.letter = letter+"";
+			return node;
+		}
+
+		if(encoding.substring(0,1).equals("0"))
+			node.left = insert(letter, encoding.substring(1), node.left);
+
+		if(encoding.substring(0, 1).equals("1"))
+			node.right = insert(letter, encoding.substring(1), node.right);
+
+		return node;
+
+	}
 }
--- Goldthwaite, Owen/src/huffman/decoding/testTree.java
+++ Goldthwaite, Owen/src/huffman/decoding/testTree.java
@@ -1,10 +1,28 @@
 package huffman.decoding;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class testTree {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException 
 	{
+		HuffmanTree oak = new HuffmanTree();
+		Scanner scan = new Scanner(new File("files/scheme.txt"));
+		Scanner cool = new Scanner(new File("files/encoded.txt"));
+		String code = scan.nextLine();
 		
+		while(scan.hasNext())
+		{
+			String scheme = scan.nextLine();
+			System.out.println(scheme);
+			oak.insert(scheme.charAt(0), scheme.substring(1));
+		}
+		
+		System.out.println(code);
+		
+		System.out.println(oak.decodeMessage(code));
 
 	}
 
