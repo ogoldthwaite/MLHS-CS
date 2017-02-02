114d9c8a41413be6306d1e51d0d28ce03bf256e1
ogoldthwaite <ogoldthwaite@10.10.125.110>
2017-02-02 10:57:46 -0500

    Huffman Decoding Stuff

---
+++ Goldthwaite, Owen/src/huffman/encoding/MessageFrequency.java
@@ -0,0 +1,50 @@
+package huffman.encoding;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class MessageFrequency 
+{
+	private Map<String, Integer> map;
+	
+	public MessageFrequency()
+	{
+		map = new HashMap<String, Integer>();
+	}
+	
+	private void createMap(String msg)
+	{
+		for(int i = 0; i < msg.length(); i++)
+		{
+			String letter = msg.charAt(i)+"";
+			if(map.containsKey(letter))
+				map.put(letter, map.get(letter) + 1);
+			else
+				map.put(letter, 1);
+		}
+	
+	}
+	
+	public PriorityQueue<FrequencyTreeNode> makeQueue(String msg)
+	{
+		createMap(msg);
+		PriorityQueue<FrequencyTreeNode> qq = new PriorityQueue<FrequencyTreeNode>();
+		
+		for(int i = 0; i < msg.length(); i++)
+		{
+			
+			FrequencyTreeNode node = new FrequencyTreeNode(msg.charAt(i), map.get(msg.charAt(i)+""));
+			qq.add(node);
+		}
+		
+		return qq;
+				
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ Goldthwaite, Owen/src/huffman/encoding/TreeTester.java
@@ -0,0 +1,13 @@
+package huffman.encoding;
+
+public class TreeTester {
+
+	public static void main(String[] args) 
+	{
+		MessageFrequency freq = new MessageFrequency();
+		System.out.println(freq.makeQueue("heeyyyy"));
+		
+
+	}
+
+}
