78b644463545f72d4cbea96f41f19383e707cf90
ogoldthwaite <ogoldthwaite@10.10.125.110>
2017-02-03 10:57:08 -0500

    Huffman encoding stuff

--- Goldthwaite, Owen/src/huffman/encoding/HuffmanTree.java
+++ Goldthwaite, Owen/src/huffman/encoding/HuffmanTree.java
@@ -10,27 +10,57 @@ public class HuffmanTree
     
     public HuffmanTree(PriorityQueue<FrequencyTreeNode> pq)
     {
-        
+        while(pq.size() > 1)
+        {
+        	FrequencyTreeNode rNode = pq.poll();
+        	FrequencyTreeNode lNode = pq.poll();
+        	FrequencyTreeNode node = new FrequencyTreeNode('-', rNode.frequency + lNode.frequency);
+        	node.right = rNode;
+        	node.left = lNode;
+        			pq.add(node);
+        }
+    	
+    	root = pq.poll();
+    	
     }
     
     public String encodeLetter(char letter)
     {
-        
+    	String returnString = encodeLetter(letter, root);
+    	
+        return returnString;
     }
     
-    private String encodeLetter(char letter, FrequencyTreeNode root)
+    private String encodeLetter(char letter, FrequencyTreeNode node)
     {
         
+    	
+    	
+    	
     }
     
     // returns a map of characters to encodings
     public Map<Character, String> encodingScheme()
     {
+        Map<Character, String> scheme = new HashMap<Character, String>();
+        
+        encodingScheme(root, "", scheme);
+        
+        return scheme;
         
     }
     
-    private void encodingScheme(FrequencyTreeNode root, String route, Map<Character, String> scheme)
+    private void encodingScheme(FrequencyTreeNode node, String route, Map<Character, String> scheme)
     {
+        if(node.letter == '-')
+        {
+        	if(node.left != null)
+        		encodingScheme(node.left, route + "0", scheme);
+        	if(node.right != null)
+        		encodingScheme(node.right, route + "1", scheme);
+        }
         
+        if(node.letter != '-')
+        	scheme.put(node.letter, route);
     }
 }
--- Goldthwaite, Owen/src/huffman/encoding/TreeTester.java
+++ Goldthwaite, Owen/src/huffman/encoding/TreeTester.java
@@ -1,12 +1,19 @@
 package huffman.encoding;
 
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class TreeTester {
 
 	public static void main(String[] args) 
 	{
 		MessageFrequency freq = new MessageFrequency();
-		System.out.println(freq.makeQueue("heeyyyy"));
+		PriorityQueue<FrequencyTreeNode> bob = freq.makeQueue("hey there");
+		System.out.println(bob.peek().letter);
 		
+		HuffmanTree spruce = new HuffmanTree(bob);
+		  Map<Character, String> scheme = spruce.encodingScheme();
+		  System.out.println(scheme);
 
 	}
 
